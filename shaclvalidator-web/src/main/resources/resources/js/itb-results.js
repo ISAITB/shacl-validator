c5903eae5937c659a52eadca1769c814a58a5eac
mfontsan <mfontsan@everis.com>
2019-06-19 14:51:20 +0200

    Remove download PDF when shapes and content

--- shaclvalidator-web/src/main/resources/resources/js/itb-results.js
+++ shaclvalidator-web/src/main/resources/resources/js/itb-results.js
@@ -16,3 +16,36 @@ function downloadResult() {
     $('#resultFormSyntax').val(syntaxType.replace("/", "_"));
     $('#resultForm').submit();
 }
+
+function downloadTypeChange(){
+	var dType = $('#downloadType').val();
+	var dSyntaxType = document.getElementById("downloadSyntaxType");
+	
+	dSyntaxType = removeOption(dSyntaxType, 'pdfType');
+	dSyntaxType = removeOption(dSyntaxType, 'notSelect');
+	
+	if(dType == "reportType"){
+		dSyntaxType.add(createOption("PDF", "pdfType"),0);
+		dSyntaxType.selectedIndex = 0;
+		
+		dSyntaxType.add(createOption("-----------------------", "notSelect"),1);
+	}
+}
+
+function removeOption(options, value){
+	for (var i=0; i<options.length; i++){
+		 if (options[i].value == value){
+			 options.remove(i);
+		 }
+	}
+	
+	return options;
+}
+
+function createOption(text, value){
+	var option = document.createElement("option");
+	option.text = text;
+	option.value = value;
+	
+	return option;
+}
