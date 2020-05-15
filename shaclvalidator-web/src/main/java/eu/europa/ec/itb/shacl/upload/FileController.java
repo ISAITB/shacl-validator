0a44b6de4d1ba31edfb475b081d26b525ff074c6
Costas Simatos <costas.simatos@sigmacubed.eu>
2020-05-15 09:43:33 +0200

    Minor fix.

--- shaclvalidator-web/src/main/java/eu/europa/ec/itb/shacl/upload/FileController.java
+++ shaclvalidator-web/src/main/java/eu/europa/ec/itb/shacl/upload/FileController.java
@@ -43,24 +43,24 @@ public class FileController {
     @GetMapping(value = "/{domain}/report", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ResponseBody
     public FileSystemResource getReport(
-    		@PathVariable String domain, 
-    		@RequestParam String id, 
-    		@RequestParam String type, 
-    		@RequestParam String syntax, 
+    		@PathVariable String domain,
+    		@RequestParam String id,
+    		@RequestParam String type,
+    		@RequestParam String syntax,
     		HttpServletResponse response) {
-        
+
     	DomainConfig domainConfig = domainConfigCache.getConfigForDomainName(domain);
         if (domainConfig == null || !domainConfig.getChannels().contains(ValidatorChannel.FORM)) {
             throw new NotFoundException();
         }
         MDC.put("domain", domain);
-        
+
         String tmpFolder = config.getTmpFolder() + "/web/" + id;
-        syntax = syntax.replace("_", "/");     
-        
+        syntax = syntax.replace("_", "/");
+
         File fileByType = getFileByType(tmpFolder, type);
         File fileOutput = null;
-        
+
         try {
         	if(fileByType.exists() && fileByType.isFile()) {
         		if(syntax.equals("pdfType")) {
@@ -72,81 +72,81 @@ public class FileController {
         		}else {
         			if(type.equals(UploadController.downloadType_content)) {
         				Lang lang = RDFLanguages.filenameToLang(fileByType.getName());
-        				
+
         				fileOutput = getReportAsSyntax(fileByType, syntax, lang.getContentType().getContentType(), tmpFolder);
         			}else {
         				fileOutput = getReportAsSyntax(fileByType, syntax, domainConfig.getDefaultReportSyntax(), tmpFolder);
         			}
         		}
-        		
+
         	}
         }catch(Exception e) {
             throw new NotFoundException();
  	   	}
 
         if(syntax.equals("pdfType")) {
-        	response.setHeader("Content-Disposition", "attachment; filename=" + type.replace("Type", "") +".pdf");        	
+        	response.setHeader("Content-Disposition", "attachment; filename=" + type.replace("Type", "") +".pdf");
         }else {
         	response.setHeader("Content-Disposition", "attachment; filename=" + type.replace("Type", "") +"." + fileManager.getLanguage(syntax));
         }
-        
+
         return new FileSystemResource(fileOutput);
     }
-    
+
     private File getFileByType(String folderName, String type) {
     	File folder = new File(folderName);
     	File file = null;
 
 		List<File> files = (List<File>) FileUtils.listFiles(folder, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
-		
+
 		for(File fileTmp : files) {
 			String filename = FilenameUtils.removeExtension(fileTmp.getName());
-			
+
 	    	switch(type) {
-	    		case UploadController.downloadType_content:	    			
+	    		case UploadController.downloadType_content:
 	    			if(filename.equals(UploadController.fileName_input)) {
 	    				file = fileTmp;
 	    			}
 	    		break;
-	    		case UploadController.downloadType_report:	    			
+	    		case UploadController.downloadType_report:
 	    			if(filename.equals(UploadController.fileName_report)) {
 	    				file = fileTmp;
 	    			}
 	    		break;
-	    		case UploadController.downloadType_shapes:	    			
+	    		case UploadController.downloadType_shapes:
 	    			if(filename.equals(UploadController.fileName_shapes)) {
 	    				file = fileTmp;
 	    			}
-	    		break;  			
+	    		break;
 	    	}
-			
+
 		}
-		
+
 		if(file==null) {
-			throw new NotFoundException();			
+			throw new NotFoundException();
 		}
-    	
+
 		return file;
-    	
+
     }
-    
+
     private File getReportAsSyntax(File reportFile, String syntax, String defaultSyntax, String tmpFolder) throws IOException {
-    	Model fileModel = JenaUtil.createMemoryModel();    	
-        fileModel.read(new FileInputStream(reportFile), null, defaultSyntax);        
+    	Model fileModel = JenaUtil.createMemoryModel();
+        fileModel.read(new FileInputStream(reportFile), null, defaultSyntax);
         String stringReport = fileManager.getShaclReport(fileModel, syntax);
-        
+
         return fileManager.getStringFile(tmpFolder, stringReport, syntax, null);
     }
 
-    
+
 	@Scheduled(fixedDelayString = "${validator.cleanupWebRate}")
 	public void removeWebFiles() {
 		logger.debug("Remove SHACL file cache");
         long currentMillis = System.currentTimeMillis();
-        
+
 		File reportFolder = new File(config.getTmpFolder()+"/web");
-		
-		if (reportFolder != null && reportFolder.exists()) {
+
+		if (reportFolder.exists()) {
             File[] files = reportFolder.listFiles();
             if (files != null) {
                 for (File file: files) {
@@ -160,10 +160,10 @@ public class FileController {
 
     private boolean handleReportFile(File file, long currentTime) {
         boolean handled = false;
-        
+
         if (file.isDirectory()) {
             handled = true;
-            
+
             if (currentTime - file.lastModified() > config.getCleanupWebRate()) {
                 FileUtils.deleteQuietly(file);
             }
