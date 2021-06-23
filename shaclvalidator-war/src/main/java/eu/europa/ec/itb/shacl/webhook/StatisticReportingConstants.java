22742c4fb719f37d272ed8c0e43e3d376801362b
Victor Marin <victor.marin.puchades@everis.com>
2021-06-23 14:28:20 +0200

    [ITB-566] Usage statistics for anonymous validation services (with country extraction)

---
+++ shaclvalidator-war/src/main/java/eu/europa/ec/itb/shacl/webhook/StatisticReportingConstants.java
@@ -0,0 +1,9 @@
+package eu.europa.ec.itb.shacl.webhook;
+
+public class StatisticReportingConstants {
+
+    public static final String WEB_API = "web";
+    public static final String SOAP_API = "soap";
+    public static final String REST_API = "rest";
+    
+}
