da1f4c243a938f5ab5e783a33ab25c21c2ef3879
simatosc <costas.simatos@sigmacubed.eu>
2026-05-07 17:50:43 +0200

    Upgrade to Java 25

--- etc/docker/shacl-validator/validator-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Default JVM options. Any option in JVM_OPTIONS that duplicates a default will
-# take precedence because it appears later on the command line (HotSpot resolves
-# duplicate -XX flags and -D system-properties in favour of the last occurrence).
-DEFAULT_JVM_OPTIONS="-XX:+ExitOnOutOfMemoryError -Djava.security.egd=file:/dev/./urandom"
-
-exec java ${DEFAULT_JVM_OPTIONS} ${JVM_OPTIONS} -jar /validator/validator.jar "$@"
