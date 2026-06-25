8d46342e4485741efa908a3fa8fd7c3a72fee8d9
simatosc <costas.simatos@sigmacubed.eu>
2026-06-25 16:44:18 +0200

    Support for looking up localised values for additionalInfo

--- shaclvalidator-common/src/main/java/eu/europa/ec/itb/shacl/validation/AdditionalInfoTemplate.java
+++ shaclvalidator-common/src/main/java/eu/europa/ec/itb/shacl/validation/AdditionalInfoTemplate.java
@@ -26,7 +26,6 @@ import org.apache.jena.vocabulary.RDF;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.regex.Pattern;
 
 /**
